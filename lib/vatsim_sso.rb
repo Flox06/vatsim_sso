ca0649141d7bd215ff3afb000876ae3ad13b743c
Pierre Ferran <pierr3@users.noreply.github.com>
2016-06-13 21:49:10 +0200

    Update vatsim_sso.rb

--- lib/vatsim_sso.rb
+++ lib/vatsim_sso.rb
@@ -6,9 +6,9 @@ module VatsimSso
 
   # provides authorize URL
   def self.login(key,cert_path,callback_url,site)
-    @consumer = OAuth::Consumer.new(key, OpenSSL::PKey::RSA.new(IO.read(cert_path)),:site => site,:request_token_path=>"/api/login_token",:access_token_path=>"/api/login_return",:authorize_path => "/auth/pre_login",:signature_method => "RSA-SHA1",:scheme => :body)
+    @@consumer = OAuth::Consumer.new(key, OpenSSL::PKey::RSA.new(IO.read(cert_path)),:site => site,:request_token_path=>"/api/login_token",:access_token_path=>"/api/login_return",:authorize_path => "/auth/pre_login",:signature_method => "RSA-SHA1",:scheme => :body)
     #@consumer.http.set_debug_output($stdout)
-    request_token = @consumer.get_request_token(
+    request_token = @@consumer.get_request_token(
         :oauth_callback => callback_url
     )
     h = JSON.parse(request_token.params.keys.first.to_s)
@@ -17,7 +17,7 @@ module VatsimSso
 
   # use it after sso callback, provides the user hash
   def self.login_return(auth_token,auth_verifier)
-    resp = OAuth::RequestToken.new(@consumer,auth_token).get_access_token(:oauth_verifier => auth_verifier)
+    resp = OAuth::RequestToken.new(@@consumer,auth_token).get_access_token(:oauth_verifier => auth_verifier)
     h = JSON.parse(resp.params.keys.first.to_s)
     h['user']
   end
